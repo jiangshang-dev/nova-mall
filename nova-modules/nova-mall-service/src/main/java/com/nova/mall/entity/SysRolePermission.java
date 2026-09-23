1545f747fcc79a00e8072d71a2009e7cab4c9b2f
lizhenghang <1135365441@qq.com>
2026-09-23 17:49:06 +0800

    1

--- nova-modules/nova-mall-service/src/main/java/com/nova/mall/entity/SysRolePermission.java
+++ nova-modules/nova-mall-service/src/main/java/com/nova/mall/entity/SysRolePermission.java
@@ -1,8 +1,13 @@
 package com.nova.mall.entity;
+
 import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
-@Data @TableName("sys_role_permission")
+
+@Data
+@TableName("sys_role_permission")
 public class SysRolePermission {
-  @TableId(type = IdType.AUTO) private Integer id;
-  private Integer roleId; private Integer permissionId;
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+    private Integer roleId;
+    private Integer permissionId;
 }
--- nova-modules/nova-mall-service/src/main/java/com/nova/mall/entity/SysUserRole.java
+++ nova-modules/nova-mall-service/src/main/java/com/nova/mall/entity/SysUserRole.java
@@ -1,8 +1,13 @@
 package com.nova.mall.entity;
+
 import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
-@Data @TableName("sys_user_role")
+
+@Data
+@TableName("sys_user_role")
 public class SysUserRole {
-  @TableId(type = IdType.AUTO) private Integer id;
-  private Integer userId; private Integer roleId;
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+    private Integer userId;
+    private Integer roleId;
 }
